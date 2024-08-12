11c8c62dd4c13d1985874dd4f43e0fa24c9b39bc
Kunal <kunalbishnoi003@gmail.com>
2024-08-12 15:40:28 +0530

    changes

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -1,11 +1,25 @@
 import Navbar from "./Navbar";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+import { useNavigate } from "react-router-dom";
 const Dashboard = () => {
+  const navigate = useNavigate();
   return (
     <>
       <Navbar backgroundcolor={true} />
-      <div className="flex items-center justify-center min-h-screen bg-priblack font-serif">
-        <h1 className="text-7xl text-primary font-sans text-white font-bold">
-          Algo Chef
+      <div className="flex items-center justify-center min-h-screen bg-priblack font-serif text-white">
+        <span className="text-7xl p-10  rounded-xl text-primary font-sans text-white font-bold">
+          Welcome to
+        </span>
+        <h1 className="text-7xl shadow-black shadow-md py-10 bg-secblack rounded-xl text-primary font-sans text-yellow-500 font-bold px-28">
+          AlgoChef <br />
+          <button
+            className="text-xl font-extralight text-white mt-5 transition-all duration-300 ease-in-out transform hover:scale-105 hover:font-semibold"
+            onClick={() => {
+              navigate("/problemset");
+            }}
+          >
+            Click here to solve problems <ArrowForwardIcon />
+          </button>
         </h1>
       </div>
     </>
