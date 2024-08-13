341edde6f830a580cf9db7c61214a1dde061afad
Kunal <kunalbishnoi003@gmail.com>
2024-08-13 20:02:19 +0530

    changes

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -1,5 +1,4 @@
 import Navbar from "./Navbar";
-import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 import { useNavigate } from "react-router-dom";
 const Dashboard = () => {
   const navigate = useNavigate();
@@ -7,20 +6,23 @@ const Dashboard = () => {
     <>
       <Navbar backgroundcolor={true} />
       <div className="flex items-center justify-center min-h-screen bg-priblack font-serif text-white">
-        <span className="text-7xl p-10  rounded-xl text-primary font-sans text-white font-bold">
+        <span className="text-7xl p-10  rounded-xl text-primary font-sans text-white font-bold  mx-4">
           Welcome to
         </span>
-        <h1 className="text-8xl shadow-black shadow-md py-10 bg-secblack rounded-xl text-primary font-sans text-yellow-500 font-bold px-28">
+        <h1
+          onClick={() => {
+            navigate("/problemset");
+          }}
+          className="text-7xl hover:cursor-pointer hover:shadow-black hover:shadow-lg duration-500 transition ease-out shadow-black shadow-md py-10 rounded-xl text-primary font-sans text-yellow-500 font-bold px-28"
+        >
           AlgoChef <br />
         </h1>
-        <button
+        {/* <button
           className="text-xl m-2 font-extralight font-sans text-white mt-5 transition-all duration-300 ease-in-out transform hover:scale-105 hover:font-semibold"
           onClick={() => {
             navigate("/problemset");
           }}
-        >
-          {"------>"}
-        </button>
+        ></button> */}
       </div>
     </>
   );
